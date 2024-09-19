fb538e921794c4d872039d33a0b43bc2cc7cb158
Don Welch <dwelch@nait.ca>
2024-09-19 11:03:18 -0600

    decimal arithmetic demo

--- ArithmeticSolution/IntroductionArithmeticExpressions/Program.cs
+++ ArithmeticSolution/IntroductionArithmeticExpressions/Program.cs
@@ -3,6 +3,8 @@ Console.WriteLine("\n\tHello, World of Math Expression!\n\n");
 
 //basic arithmetic operators
 
+//Using Integers
+Console.WriteLine("\n\tInteger Arithmetic expressions\n");
 int intA = 3, intB = 4, intC = 0;
 
 //addition
@@ -30,3 +32,33 @@ intC = intA % intB;
 Console.WriteLine($"\nThe result of the math expression {intA} % {intB} is: {intC}\n");
 intC = intB % intA;
 Console.WriteLine($"\nThe result of the math expression {intB} % {intA} is: {intC}\n");
+
+//***********************************************************************************
+
+//Using Decimals
+Console.WriteLine("\n\n\tDecimal Arithmetic expressions\n");
+double doubleA = 3, doubleB = 4, doubleC = 0;
+
+//addition
+doubleC = doubleA + doubleB;
+Console.WriteLine($"\nThe result of the math expression {doubleA} + {doubleB} is: {doubleC}\n");
+
+//subtraction
+doubleC = doubleA - doubleB;
+Console.WriteLine($"\nThe result of the math expression {doubleA} - {doubleB} is: {doubleC}\n");
+
+//Multiplication
+doubleC = doubleA * doubleB;
+Console.WriteLine($"\nThe result of the math expression {doubleA} * {doubleB} is: {doubleC}\n");
+
+//Division
+doubleC = doubleA / doubleB;
+Console.WriteLine($"\nThe result of the math expression {doubleA} / {doubleB} is: {doubleC}\n");
+doubleC = doubleB / doubleA;
+Console.WriteLine($"\nThe result of the math expression {doubleB} / {doubleA} is: {doubleC}\n");
+
+//Modular division
+doubleC = doubleA % doubleB;
+Console.WriteLine($"\nThe result of the math expression {doubleA} % {doubleB} is: {doubleC}\n");
+doubleC = doubleB % doubleA;
+Console.WriteLine($"\nThe result of the math expression {doubleB} % {doubleA} is: {doubleC}\n");
