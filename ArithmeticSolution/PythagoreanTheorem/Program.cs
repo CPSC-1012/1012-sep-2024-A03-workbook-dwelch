a5acd30cef9c79bdac4cc95bddac25e980309084
Don Welch <dwelch@nait.ca>
2024-09-20 09:50:38 -0600

    demo of degguer

--- ArithmeticSolution/PythagoreanTheorem/Program.cs
+++ ArithmeticSolution/PythagoreanTheorem/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("\n\tPythagorean Theorem calculated using Math class\n");
 
 const double POWER_OF_TWO = 2.0;
-double height, baseLength, hypotenuse;
+double height, baseLength = 0, hypotenuse;
 string inputValue;
 double measurementsSquared = 0.0;
 
@@ -19,9 +19,9 @@ baseLength = double.Parse(inputValue);
 // * and / left to right
 // + and - left to right
 
-//measurementsSquared = (height * height);
-//measurementsSquared = measurementsSquared + Math.Pow(baseLength, POWER_OF_TWO);
-//hypotenuse = Math.Sqrt(measurementsSquared);
+measurementsSquared = (height * height);
+measurementsSquared = measurementsSquared + Math.Pow(baseLength, POWER_OF_TWO);
+hypotenuse = Math.Sqrt(measurementsSquared);
 
 //measurementsSquared = (height * height);
 //measurementsSquared += Math.Pow(baseLength, POWER_OF_TWO);
@@ -31,7 +31,7 @@ baseLength = double.Parse(inputValue);
 //hypotenuse = Math.Sqrt(measurementsSquared);
 //Refactoring
 
-hypotenuse = Math.Sqrt((height * height) + (Math.Pow(baseLength, POWER_OF_TWO)));
+//hypotenuse = Math.Sqrt((height * height) + (Math.Pow(baseLength, POWER_OF_TWO)));
 
 Console.WriteLine($"\nthe hypotenuse of height: {height} and base {baseLength} is: " +
     $"{hypotenuse}");
