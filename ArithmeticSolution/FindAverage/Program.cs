e8362f122dc4d7400380d2d8373a3a9609f5d5ee
Don Welch <dwelch@nait.ca>
2024-09-20 08:25:56 -0600

    demo constants in C#

--- ArithmeticSolution/FindAverage/Program.cs
+++ ArithmeticSolution/FindAverage/Program.cs
@@ -1,6 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("\n\tIt is just an Average World!\n");
 
+//constants
+//a constant is a data storage item that has a program definted name
+//                                       has a datatype
+//                                       has an assigned value
+//a constant, unlike a variable, CANNOT be altered
+//a constant can be used in your code just like a variable
+const int NUMBER_OF_VALUES = 3;
+
 double calculateAverage = 0.0;
 double numOne, numTwo, numThree;
 string inputValue;
@@ -19,7 +27,7 @@ numThree = double.Parse(inputValue);
 // (....)
 // * and / left to right
 // + and - left to right
-calculateAverage = (numOne + numTwo + numThree) / 3;
+calculateAverage = (numOne + numTwo + numThree) / NUMBER_OF_VALUES;
 
 Console.WriteLine($"\nThe average of ({numOne}, {numTwo}, {numThree}) is {calculateAverage}");
 Console.WriteLine($"\nThe average of ({numOne}, {numTwo}, {numThree}) is {calculateAverage.ToString("F2")}");
