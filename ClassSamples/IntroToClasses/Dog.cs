47491fad627900364106bc85492a4a3cb197a047
Don Welch <dwelch@nait.ca>
2024-11-18 14:43:34 -0700

    Introduction to classes, class syntax, dagta members, behaviours, namespaces

---
+++ ClassSamples/IntroToClasses/Dog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//when you create a class within a project, by default,
+//  the namespace for the class is the project name
+
+namespace Animals
+{
+    //the definition of the class name
+    //  a) access level
+    //       private nothing outside of the class has acces to items
+    //          within the class
+    //       internal access to the class is limited to programming
+    //          within the project
+    //       public access to the class is open to an outside user
+    //          of the class
+    //  b) the key word class
+    //  c) the programmer/developer define name of the class
+    public class Dog
+    {
+        //declares the characteristics of a Dog
+        //each characteristic is a variable
+        //a valid characteristic has a valid C# datatype
+        //NOTE: a class can have various datatypes
+
+        //Data Members (aka fields, variables, value,....)
+
+        //if you wish to encapsulate your data in a secure fashion
+        //  your characteristics have an access type of private
+
+        private string _Name;
+        private double _Age;
+
+        //Behaviours (aka methods)
+
+        //mutator (setter)
+        public void SetName(string name)
+        {
+            _Name = name;
+        }
+
+        //accessor (getter)
+        public string GetName()
+        {
+            return _Name;
+        }
+    }
+}
---
+++ ClassSamples/IntroToClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Animals;
+
+Console.WriteLine("\n\tHello, World of Classes!\n");
+
+//the class standard for coding classes will be
+//      each class definition will be coded in a separate physical file
+
+//to use a class in your program you delcare your instance (object)
+//  using the class name.
+
+Dog myDog; //this is a variable capable of holding an instance of Dog
+
+//to create an instance (object of) Dog, you will ask the system
+//  to create a new instance
+myDog = new Dog();
+
+//to access an item within your instance your code will use the dot (.) operate
+//assigning a value to the public characteristic _Name (using a mutator)
+myDog.SetName("No");
+
+//access data within the current instance one could use the characteristic
+//  name if the access level is public (using an accessor)
+Console.WriteLine($"My dog is called {myDog.GetName()}");
+Console.WriteLine($"My dog is {myDog.????} years old");
+
+
